5c3f5f055f5ff4b06ad267a3d6433e5bd2030657
vaishnavigupta20 <guptavaishu2005@gmail.com>
2026-03-27 11:15:27 +0530

    Added deployment files

--- backend/app.py
+++ backend/app.py
@@ -314,5 +314,6 @@ def uploads(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
